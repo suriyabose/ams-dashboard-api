97b79f1721de3cc09ee9aff59078c7adbc886d4d
suriyabose <suriyakumarbose@gmail.com>
2022-06-01 16:32:53 +0530

    init_commit

---
+++ src/main/java/com/example/amsdashboardapi/AmsDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amsdashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsDashboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amsdashboardapi/AmsDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amsdashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsDashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
